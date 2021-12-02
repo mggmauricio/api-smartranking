6d4c46b4b539153ff6656d76e8bfa5d2da48ca45
Mauricio <mauriciomgodoy01@gmail.com>
2021-12-02 16:37:43 -0300

    first commit

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto'
+import { JogadoresService } from './jogadores.service'
+import { Jogador } from './interfaces/jogador.interface'
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+
+    constructor(private readonly jogadoresService: JogadoresService) {}
+
+    
+    @Post()
+    async criarAtualizarJogador(
+        @Body() criaJogadorDto: CriarJogadorDto) {
+            await this.jogadoresService.criarAtualizarJogador(criaJogadorDto)
+    }
+
+    @Get()
+    async consultarJogadores() : Promise<Jogador[]> {
+        return this.jogadoresService.consultarTodosJogadores();
+    }
+
+    @Delete()
+    async deletaJogador: Promise<void> {
+        await this.jogadoresService.deletaJogador();
+
+    }
+}
